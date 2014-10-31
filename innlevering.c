07c1cb305fe0f82e6fde0860c5dc181c1cfa8109
Pal Sivertsen <palmarius@hotmail.com>
2014-10-31 20:51:10 +0100

    formatKey function now filters away any non [a-z] char

--- innlevering.c
+++ innlevering.c
@@ -19,7 +19,7 @@ int encode(const char *inputMessageFile, const char *keyFile,
     char *encodedStream) {
   char *pFormattedKey = malloc(0);
   // TODO Insert correct values
-  formatKey("ABCABCABC", pFormattedKey);
+  formatKey("ABCABCABC. ,", pFormattedKey);
   printf("Formatted key:\t%s\n\n", pFormattedKey);
 
   free(pFormattedKey);
@@ -28,23 +28,26 @@ int encode(const char *inputMessageFile, const char *keyFile,
 int formatKey(const char *key, char *formattedKey) {
   int numChars = 4098;
 
-  int i;
-  for (i = 0; key[i]; i++ ) {
+  int currentIndexInFormattedKey = 0;
+  for (int i = 0; key[i]; i++ ) {
     if (numChars < i) {
       numChars += 4098;
       formattedKey = realloc(formattedKey, sizeof(char)*numChars);
     }
     char currentChar = key[i];
+    // TODO Use string lib
     // If uppercase
     if (currentChar >= 'A' && currentChar <= 'Z') {
       // Switch casing
       currentChar ^= ' ';
     }
-    formattedKey[i] = currentChar;
+    if (currentChar >= 'a' && currentChar <= 'z') {
+      formattedKey[currentIndexInFormattedKey++] = currentChar;
+    }
     printf("New char: %c\n", currentChar);
   }
   // Resize formattedKey
-  formattedKey = realloc(formattedKey, sizeof(char)*i);
+  formattedKey = realloc(formattedKey, sizeof(char)*currentIndexInFormattedKey);
   printf("formatted: %s\n", formattedKey);
   return 0;
 }
