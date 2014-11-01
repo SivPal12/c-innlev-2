ce2e980fbba884e5406df9f9d6d63e465b5ff5b9
Pal Sivertsen <palmarius@hotmail.com>
2014-11-01 22:26:32 +0100

    Implemented encoding

--- innlevering.c
+++ innlevering.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 
 int encode(const char *inputMessageFile, const char *keyFile,
     char *encodedStream);
@@ -7,6 +10,8 @@ int encode(const char *inputMessageFile, const char *keyFile,
 int formatKey(const char *keyFile, char *formattedKey);
 
 char *readFile(const char *file);
+
+int encodeChar(char charToEncode, char *formattedKey, char *encodedChar, int keyIndex);
 
 int main (int argc, char *argv[]){
   char *toEncode = "abc";
@@ -25,7 +30,29 @@ int encode(const char *inputMessageFile, const char *keyFile,
   char *pFormattedKey = malloc(0);
   // TODO Insert correct values
   formatKey(rawKey, pFormattedKey);
-  printf("Formatted key:\t%s\n\n", pFormattedKey);
+  printf("Formatted key:\n%s\n\n", pFormattedKey);
+
+  char *msgToEncode = "There is a consert.....";
+  int msgIndex = 0, encodedSize = 0, keyIndex = 0, bufferSize = 0;
+  while (msgToEncode[msgIndex] != '\0') {
+    char *encodedChar = malloc(0);
+    keyIndex = encodeChar(msgToEncode[msgIndex], pFormattedKey, encodedChar, keyIndex);
+    int encodedCharLength = strlen(encodedChar);
+    encodedSize += encodedCharLength;
+    if (bufferSize <= encodedSize) {
+      bufferSize += 4098;
+      // TODO handle failed
+      encodedStream = realloc(encodedStream, sizeof(char)*bufferSize);
+    }
+    strcat(encodedStream, encodedChar);
+    free(encodedChar);
+    msgIndex++;
+  }
+
+  // TODO Handle failed
+  encodedStream = realloc(encodedStream, sizeof(char)*encodedSize);
+
+  printf("Encoded:\n%s\n", encodedStream);
 
   free(pFormattedKey);
   free(rawKey);
@@ -85,4 +112,41 @@ char *readFile(const char *file) {
   fclose(pFile);
   buffer[fileSize-1] = '\0';
   return realloc(buffer, fileSize - 1);
+}
+
+int encodeChar(char charToEncode, char *key, char *encodedChar, int keyIndex) {
+printf("To encode: %c\n", charToEncode);
+  if (!isalpha(charToEncode)) {
+    encodedChar = realloc(encodedChar, sizeof(char)*2);
+    char buffer[2];
+    sprintf(buffer, "%c", charToEncode);
+    strcpy(encodedChar, buffer);
+    return keyIndex;
+  }
+  int keyLength = strlen(key);
+
+  while (tolower(charToEncode) != key[keyIndex]) {
+    if (keyIndex++ >= keyLength) {
+      keyIndex = 0;
+    }
+  }
+
+  encodedChar = realloc(encodedChar, sizeof(int)+4);
+  char *buffer;
+
+printf("PreS size: %lu\n", sizeof(encodedChar));
+  if (islower(charToEncode)) {
+//    strcpy(encodedChar, "[%d]");
+    buffer = "[%d]";
+  } else {
+//    strcpy(encodedChar, "[-%d]");
+    buffer = "[-%d]";
+  }
+printf("Post size: %lu\n", sizeof(encodedChar));
+
+//  sprintf(encodedChar, encodedChar, keyIndex+1);
+  sprintf(encodedChar, buffer, keyIndex+1);
+printf("Encoded char: %s\n", encodedChar);
+
+  return keyIndex;
 }
